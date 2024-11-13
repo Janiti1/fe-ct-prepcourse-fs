7de7ec86befb05018903499473b96cb2e40f3796
jano <garciajano07@gmail.com>
2024-11-13 03:50:59 -0300

    bucles te odio

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -2,7 +2,11 @@ function obtenerMayor(x, y) {
   // "x" e "y" son números enteros.
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
-  // Tu código:
+  // Tu código: 
+    if (x >= y) 
+      return x;  
+     else 
+      return y;
 }
 
 module.exports = obtenerMayor;
--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,21 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
-}
+  let producto = 1;  
+
+ 
+  if (a > b) {  
+    [a, b] = [b, a]; 
+  }  
+
+ 
+  for (let i = a; i <= b; i++) {  
+    producto *= i; 
+    if (producto == 0)
+      return 0
+  }  
+
+  return producto;
+}  
 
 module.exports = productoEntreNúmeros;
